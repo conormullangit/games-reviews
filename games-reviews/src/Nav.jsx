3a585178a02eb7277b2cc34fd121d4d97d5cc93e
Mullanator <cmullan@gmail.com>
2021-09-03 11:27:14 +0100

    starting new nav bar and categories page

--- games-reviews/src/Nav.jsx
+++ games-reviews/src/Nav.jsx
@@ -7,54 +7,55 @@ import * as AiIcons from 'react-icons/ai';
 import { NavSide } from './NavSide';
 import { IconContext } from 'react-icons/lib';
 
-const Nav = () => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const showSidebar = () => setSidebar(!sidebar)
-
-  return (
-    <>
-      <IconContext.Provider value={{ color: "black" }}>
-        <div className="navbar">
-          <Link to="#" className="menu-bar">
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
-        </div>
-        <nav className={sidebar ? "nav-menu__active" : "nav-menu"}>
-          <ul className="nav-manu-items">
-            <li className="navbar-toggle">
-              <Link to="#" className="menu-bars">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {NavSide.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
-  );
-};
-
-export default Nav;
-
 // const Nav = () => {
+//   const [sidebar, setSidebar] = useState(false);
+
+//   const showSidebar = () => setSidebar(!sidebar)
 
 //   return (
 //     <>
-//       <div className="menu-btn">
-//         <div className="menu-btn__burger"></div>
-//       </div>
-
-//       <NavLink to="/api/reviews">Home</NavLink>
+//       <IconContext.Provider value={{ color: "black" }}>
+//         <div className="navbar">
+//           <Link to="#" className="menu-bar">
+//             <FaIcons.FaBars onClick={showSidebar} />
+//           </Link>
+//         </div>
+//         <nav className={sidebar ? "nav-menu__active" : "nav-menu"}>
+//           <ul className="nav-manu-items">
+//             <li className="navbar-toggle">
+//               <Link to="#" className="menu-bars">
+//                 <AiIcons.AiOutlineClose />
+//               </Link>
+//             </li>
+//             {NavSide.map((item, index) => {
+//               return (
+//                 <li key={index} className={item.cName}>
+//                   <Link to={item.path}>
+//                     {item.icon}
+//                     <span>{item.title}</span>
+//                   </Link>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </nav>
+//       </IconContext.Provider>
 //     </>
 //   );
 // };
+
+const Nav = () => {
+
+  return (
+    <>
+      <div className="menu-btn">
+        <div className="menu-btn__burger"></div>
+      </div>
+
+      <NavLink to="/api/reviews">Home</NavLink>
+      <NavLink to="/api/categories">Categories</NavLink>
+      <NavLink to="/api/users/jessjelly">Users</NavLink>
+    </>
+  );
+};
+export default Nav;
